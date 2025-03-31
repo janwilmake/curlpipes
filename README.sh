743c38a35d5990be7422e12ff718727eccfdc839
Jan Wilmake <jan@wilmake.com>
2025-03-31 10:23:14 +0200

    improvements

--- README.sh
+++ README.sh
@@ -8,3 +8,5 @@ curl -s https://xymake.com/janwilmake/status/1906585720447189401 | curl -d @- -d
 # the node.js demo shows that the request is actually comprised of the url and some headers for the host and credentials!
 # with this in mind, the behavior of workerd makes more sense, but it's still annoying that they DO add the hostname to the request.url, but they forgot the basic credentials! 
 # run node demo with 'node node-demo.js'
+
+# Great explanation: https://github.com/cloudflare/workerd/issues/194
