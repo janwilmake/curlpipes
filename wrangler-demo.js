a21a0b6ff461ed2b262f7a5521d1288277d5432a
Jan Wilmake <jan@wilmake.com>
2025-03-31 10:33:21 +0200

    improvements

--- wrangler-demo.js
+++ wrangler-demo.js
@@ -7,15 +7,13 @@ export default {
   fetch: async (request) => {
     // This is too annoying! Should just work with the first URL if I pass username:password in the URL
     const authorization = request.headers.get("Authorization");
-    const firstUrl = new URL(request.url);
-    const credentials = authorization.startsWith("Basic ")
-      ? atob(authorization.slice("Basic ".length))
-      : undefined;
-    const url = credentials
-      ? new URL("https://" + credentials + "@" + firstUrl.host, firstUrl)
-      : firstUrl;
-    const username = url.username;
-    const password = url.password;
+    const url = new URL(request.url);
+    const [username, password] = authorization.startsWith("Basic ")
+      ? atob(authorization.slice("Basic ".length)).split(":")
+      : [];
+    url.username = username;
+    url.password = password;
+
     console.log("URL credentials:", JSON.stringify({ username, password }));
 
     // THIS WORKS GREAT!
