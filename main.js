91db5763c07fa79a789ed17af914d074292084b6
Jan Wilmake <jan@wilmake.com>
2025-03-31 09:26:52 +0200

    improvements

--- main.js
+++ main.js
@@ -8,11 +8,12 @@ export default {
     const url = new URL(request.url);
     const username = url.username;
     const password = url.password;
-
+    // NB: This does NOT WORK!!!!!
     console.log("URL username:", username);
     console.log("URL password:", password);
     console.log("Full URL:", request.url);
 
+    // THIS WORKS GREAT!
     const text = await request.text();
     console.log(text);
     return new Response("You entered text with " + text.length + " chars");
